dbe4843c9839afc68013c8bbca879e81d6a581a6
Pavel <SmetaninPavel.rus@gmail.com>
2014-05-02 23:36:40 +0600

    Update Design Version 2.0

---
+++ js/main.js
@@ -0,0 +1,63 @@
+;
+function inArray(value, array)
+{
+	for(var i = 0; i < array.length; i++)
+	{
+		if(array[i] == value)
+			return true;
+	}
+	return false;
+}
+
+var expandArray = document.getElementsByClassName('expand');
+var activePost = "active-post";
+for (var i = 0; i < expandArray.length; i++)
+{
+	expandArray[i].onclick = function()
+	{
+		var post = this.parentNode;
+		var activePostsArray = document.getElementsByClassName(activePost);
+		for (var j = 0; j < activePostsArray.length; j++)
+		{
+			if (activePostsArray[j] === post)
+				continue;
+
+			var readFull = activePostsArray[j].getElementsByClassName("read-full");
+			readFull[0].classList.remove("hidden");
+
+			activePostsArray[j].classList.remove(activePost);
+		}
+
+		if (inArray(activePost, post.classList))
+		{
+			post.classList.remove(activePost);
+
+			var readFull = post.getElementsByClassName("read-full");
+			readFull[0].classList.remove("hidden");
+		}
+		else
+		{
+			post.classList.add(activePost);
+
+			var readFull = post.getElementsByClassName("read-full");
+			readFull[0].classList.add("hidden");
+		}
+	};
+}
+
+// ******************************* Создаём конструктор класса
+
+var Post = function(titleName, date, category, preview, article)
+{
+	this.titleName = titleName;
+	this.date = date;
+	this.category = category;
+	this.preview = preview;
+	this.article = article;
+}
+
+var request = new XMLHttpRequest();
+request.open("GET", "data/0.json", false);
+request.send(null);
+var post = JSON.parse(request.responseText);
+console.log(post);
